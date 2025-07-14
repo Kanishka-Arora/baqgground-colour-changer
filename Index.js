a847ecfd3615fdf5d55bed9c88088bc58c287bbe
Kanishka-Arora <kanishkaarora42041@gmail.com>
2025-07-14 19:10:49 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,11 @@
+document.getElementById("red").onclick = function(){
+    document.body.style.backgroundColor = "red";
+    };
+
+    document.getElementById("blue").onclick = function(){
+    document.body.style.backgroundColor = "blue";
+    };
+
+    document.getElementById("green").onclick = function(){
+    document.body.style.backgroundColor = "green";
+    };
